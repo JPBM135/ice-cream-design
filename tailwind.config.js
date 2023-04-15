2bceaf40eccb4bba32a01d5d24e0f48917f3ca58
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-04-15 18:55:57 -0300

    feat(*): repo config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -12,6 +12,15 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        bg_color: "#ffe4af",
+        bg_color_secondary: "#947a51",
+        bg_color_tertiary: "#976c29",
+        paragraph_color: "#231f20",
+        paragraph_color_secondary: "#222221",
+        primary: "#c74664",
+        secondary: "#a8e9d6",
+      }
     },
   },
   plugins: [],
