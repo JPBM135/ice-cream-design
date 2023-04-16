bf6cc708926fb8c6d2bac8c3d5d46e1320962384
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-04-16 16:44:54 -0300

    feat(routes):  404 pages

---
+++ app/components/404.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export function NotFoundComponent(): JSX.Element {
+	return (
+		<div className="flex justify-center gap-6">
+			<div className="my-auto p-10 border-4 border-dashed rounded-2xl border-primary">
+				<h1 className="text-9xl xl:text-[10rem] font-extrabold text-center font-cookie text-primary">404 :(</h1>
+				<h2 className="text-xl xl:text-3xl font-bold text-center text-secondary">Ops, algo deu errado!</h2>
+			</div>
+			<Image alt="Authentic Gourmet Logo" className="my-auto" height={700} src="/falled-ice-cream.svg" width={700} />
+		</div>
+	);
+}
