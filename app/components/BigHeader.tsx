81e24f2d7aa26430292706c39a15df7c19264ee5
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-05-18 02:43:26 -0300

    feat(root): use custom header and boilerplate

---
+++ app/components/BigHeader.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import { GitBranch, IceCream, IdentificationCard, Info, Storefront, UsersThree } from '@phosphor-icons/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import { VerticalDivider } from './VerticalDivider';
+
+interface BigImageHeaderProps {
+	image: string;
+	subtitle: string;
+	title: string;
+}
+
+export function BigImageHeader(): JSX.Element {
+	const [studentsOpen, setStudentsOpen] = useState(false);
+
+	const glowTailwindClass = 'text-secondary glow';
+
+	const student = [
+		'João Pedro Borges Martins',
+		'Matheus Ricardo S. Viela',
+		'Gabriela Carolini Paliuco',
+		'Brenda Ferreira dos Santos',
+		'Yohanna A. Lellis',
+		'Ana Lívia Ribeiro',
+	].sort((a, b) => a.localeCompare(b));
+
+	return (
+		<div className="relative">
+			<div className="flex justify-between items-center pr-16 w-full h-[10vh] bg-bg_color_tertiary">
+				<div className="bg-bg_color_tertiary p-5 rounded-r-lg">
+					<Image alt="Authentic Gourmet Logo" className="mt-52" height={250} src="/logo.png" width={250} />
+				</div>
+				<nav className="flex space-x-4 gap-5">
+					<Link className={`text-white hover:border-b-primary flex pb-2 border-b-white border-b `} href="/store">
+						<Storefront size={24} weight="bold" />
+						<VerticalDivider />
+						Nossa Loja
+					</Link>
+					<Link className={`text-white hover:border-b-primary flex pb-2 border-b-white border-b `} href="/about">
+						<Info size={24} weight="bold" />
+						<VerticalDivider />
+						Sobre Nós
+					</Link>
+					<Link className="text-white hover:border-b-primary flex pb-2 border-b-white border-b" href="/flavors">
+						<IceCream size={24} weight="bold" />
+						<VerticalDivider />
+						Sabores
+					</Link>
+					<Link className="text-white hover:border-b-primary flex pb-2 border-b-white border-b" href="/make-your">
+						<IceCream size={24} weight="bold" />
+						<VerticalDivider />
+						Monte o seu
+					</Link>
+					<Link
+						className="text-white hover:border-b-primary flex pb-2 border-b-white border-b"
+						href="https://github.com/JPBM135/ice-cream-design"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						<GitBranch size={24} weight="bold" />
+						<VerticalDivider />
+						GitHub
+					</Link>
+					<div
+						className={`cursor-pointer text-white hover:border-b-primary flex pb-2 border-b-white border-b ${
+							studentsOpen ? glowTailwindClass : ''
+						}`}
+						onClick={() => setStudentsOpen(!studentsOpen)}
+					>
+						<UsersThree size={24} weight="bold" />
+						<VerticalDivider />
+						Participantes
+					</div>
+					{studentsOpen && (
+						<div className="absolute top-20 right-2 bg-bg_color rounded-md shadow-md p-4 border-gray-900 border-2">
+							<div className="flex items-center space-x-2 text-gray-800 border-b border-gray-300" />
+							{student.map((student) => (
+								<div
+									className="flex gap-1 items-center font-medium space-x-1 border-b border-gray-300 py-2"
+									key={student}
+								>
+									<IdentificationCard size={24} />
+									<span className="font-nunito font-semibold">{student}</span>
+								</div>
+							))}
+						</div>
+					)}
+				</nav>
+			</div>
+		</div>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,45 @@
+import { BigImageHeader } from './components/BigHeader';
 import { Header } from './components/Header';
 
 export default function Home() {
 	return (
-		<div className="w-full h-fit">
-			<Header />
-			<main className="w-full h-[10000px] bg-gradient-to-b from-bg_color to-bg_color_secondary" />
+		<div className="w-full h-full bg-gradient-to-b from-bg_color to-bg_color_secondary">
+			<BigImageHeader />
+			<main className="w-full h-[96vh] flex flex-col items-center gap-10">
+				<div className="mt-10 p-5 border-4 border-dashed border-primary rounded-2xl">
+					<h1 className="text-5xl font-bold text-center text-white px-4 py-2 mx-36 mb-5 font-cookie">
+						Authentic Gourmet
+					</h1>
+					<h3 className="text-2xl font-bold text-center text-white">
+						Sorvetes irresistíveis que fazem seu paladar dançar de alegria!
+					</h3>
+				</div>
+
+				<article className="w-3/6 flex flex-col justify-center gap-5 px-5 py-5 bg-secondary rounded-xl">
+					<p>
+						Bem-vindos à Sorveteria <span className="text-primary">Authentic!</span>
+					</p>
+
+					<p>
+						Na Sorveteria Authentic, a doçura se torna realidade. Somos mais do que apenas uma sorveteria; somos um
+						refúgio de sabores autênticos, momentos felizes e lembranças doces. Aqui, mergulhe em um mundo de delícias
+						geladas que vão encantar seus sentidos e alegrar seu coração.
+					</p>
+
+					<p>
+						Sinta o sabor autêntico em cada colherada. Nossos sorvetes são cuidadosamente preparados com ingredientes da
+						mais alta qualidade, proporcionando uma explosão de sabores deliciosos. Desde os clássicos como baunilha e
+						chocolate até as criações mais ousadas, como pistache com framboesa, nossa variedade de opções atende a
+						todos os paladares.
+					</p>
+
+					<p>
+						Acreditamos que a sorveteria é um lugar para todas as idades. Das crianças aos mais velhos, todos são
+						bem-vindos em nossa atmosfera calorosa e acolhedora. Crie momentos doces com sua família e amigos,
+						compartilhando sorvetes gigantes, casquinhas coloridas e taças cheias de felicidade.
+					</p>
+				</article>
+			</main>
 		</div>
 	);
 }
