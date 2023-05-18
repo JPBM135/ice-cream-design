59c005b8a049598e7af942f3ce17f5c09f3584d4
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-05-18 02:45:23 -0300

    chore(flavors): only display flavors

--- app/flavors/page.tsx
+++ app/flavors/page.tsx
@@ -1,116 +1,23 @@
 'use client';
 
-import { Header } from '@app/app/components/Header';
-import rawFlavors from '@app/public/balls.json' assert { type: 'json' };
+import flavors from '@app/public/balls.json' assert { type: 'json' };
 import Image from 'next/image';
-import { useMemo, useState } from 'react';
-import { useWindowSize } from '../utils/useWindow';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { Header } from '../components/Header';
 import { createStorageBucketUrl } from '../utils/utils';
 
-const MAX_SELECTED = 4;
-
-interface Flavor {
-	calories: number | null;
-	description: string | null;
-	id: string;
-	name: string;
-	namePtBr: string;
-	selected: boolean;
-}
-
-export default function FlavorsPage() {
-	const [flavors, setFlavors] = useState<Flavor[]>(
-		rawFlavors.map<Flavor>((flavor) => ({ ...flavor, selected: false })),
-	);
-
-	const size = useWindowSize();
-
-	const selectedCount = () => flavors.filter((flavor) => flavor.selected).length;
-
-	const toggleFlavor = (flavorId: string, isSelected: boolean) => {
-		if (!isSelected && selectedCount() >= MAX_SELECTED) {
-			return;
-		}
-
-		setFlavors((flavors) =>
-			flavors.map((flavor) => {
-				if (flavor.id === flavorId) {
-					return { ...flavor, selected: !flavor.selected };
-				}
-
-				return flavor;
-			}),
-		);
-	};
-
-	const desselectAll = () => {
-		setFlavors((flavors) => flavors.map((flavor) => ({ ...flavor, selected: false })));
-	};
-
-	const coneSize = useMemo(() => {
-		if (!size.height) {
-			return 200;
-		}
-
-		if (size.height <= 720) {
-			return 150;
-		}
-
-		if (size.height <= 1_080) {
-			return 200;
-		}
-	}, [size]);
-
-	const selectedIceCreamSize = useMemo(() => {
-		if (!size.height) {
-			return 200;
-		}
-
-		if (size.height! <= 720) {
-			return 200;
-		}
-
-		if (size.height! <= 1_080) {
-			return 300;
-		}
-	}, [size]);
-
-	const iceCreamBallsFormula = useMemo(() => {
-		if (!size.height) {
-			return (index: number) => (index + 1) * 100 + 340;
-		}
-
-		if (size.height <= 720) {
-			return (index: number) => (index + 1) * 70 + 310;
-		}
-
-		if (size.height <= 1_080) {
-			return (index: number) => (index + 1) * 100 + 340;
-		}
-
-		return (index: number) => (index + 1) * 100 + 340;
-	}, [size]);
-
-	console.log('flavors', {
-		window: globalThis?.window,
-		innerHeight: globalThis?.window?.innerHeight,
-		coneSize,
-		selectedIceCreamSize,
-		iceCreamBallsFormula: iceCreamBallsFormula?.toString(),
-	});
-
+export default function Home() {
 	return (
 		<div className="w-full h-fit">
 			<Header allowBiggerLogo={false} />
-			<main className="w-full h-full flex flex-row bg-gradient-to-b from-bg_color to-bg_color_secondary">
+			<main className="w-full h-full bg-gradient-to-b from-bg_color to-bg_color_secondary">
 				<div className="w-[inherit] flex flex-wrap h-auto justify-evenly">
 					{flavors.map((flavor) => (
-						<div
-							className={`cursor-pointer w-72 h-72 m-12 align-middle rounded-2xl ${
-								flavor.selected ? 'bg-primary' : 'bg-bg_color_tertiary'
-							}`}
+						<Link
+							className="cursor-pointer w-72 h-72 m-12 align-middle rounded-2xl hover:animate-pulse bg-bg_color_tertiary"
+							href={`/flavors/${flavor.id.replace('.png', '')}`}
 							key={flavor.id}
-							onClick={() => toggleFlavor(flavor.id, flavor.selected)}
 						>
 							<Image
 								alt={flavor.name}
@@ -122,76 +29,8 @@ export default function FlavorsPage() {
 								width={280}
 							/>
 							<span className="text-white font-medium text-center block">{flavor.namePtBr}</span>
-						</div>
+						</Link>
 					))}
-				</div>
-				<div className="w-[600px] h-[93vh] pb-5 bg-secondary sticky right-0 top-[5rem] flex flex-col">
-					{flavors
-						.filter((flavor) => flavor.selected)
-						.map((flavor, index) => (
-							<div
-								className={index === 0 ? 'mb-[-75px]' : 'my-[-75px]'}
-								key={flavor.id}
-								style={{
-									zIndex: -1 * index + 4,
-								}}
-							>
-								<Image
-									alt={flavor.name}
-									className="mx-auto mb[-10px]"
-									height={selectedIceCreamSize ?? 300}
-									key={flavor.id}
-									loading="lazy"
-									src={createStorageBucketUrl(flavor.id)}
-									width={selectedIceCreamSize ?? 300}
-								/>
-							</div>
-						))}
-					<Image
-						alt="Ice Cream Cone"
-						className={selectedCount() ? 'm-auto mt-[-20px]' : 'm-auto'}
-						height={coneSize ?? 200}
-						loading="lazy"
-						src="/ice-cream-cone.png"
-						width={coneSize ?? 200}
-					/>
-					<div className="w-full flex flex-row justify-center items-center gap-4">
-						<div className="flex flex-row justify-center items-center px-3 py-2 mb-3 border border-dashed border-primary rounded-xl text-primary font-bold">
-							<div
-								className={`w-8 h-8 rounded-full border-2 border-primary mx-1 ${
-									selectedCount() >= 1 ? 'bg-primary' : ''
-								}`}
-							/>
-							<div
-								className={`
-								w-8 h-8 border-2 rounded-full border-primary mx-1
-								${selectedCount() >= 2 ? 'bg-primary' : ''}
-							`}
-							/>
-							<div
-								className={`
-								w-8 h-8 border-2 rounded-full border-primary mx-1
-								${selectedCount() >= 3 ? 'bg-primary' : ''}
-							`}
-							/>
-							<div
-								className={`
-								w-8 h-8 rounded-full border-2 border-primary mx-1
-								${selectedCount() >= 4 ? 'bg-primary' : ''}
-							`}
-							/>
-						</div>
-						<button
-							className="
-							items-center px-3 py-2 mb-3 border border-primary rounded-xl text-primary font-bold font-nunito transition duration-300 ease-in-out hover:bg-primary hover:text-white
-							"
-							disabled={selectedCount() === 0}
-							onClick={() => desselectAll()}
-							type="button"
-						>
-							Deselecionar Todos
-						</button>
-					</div>
 				</div>
 			</main>
 		</div>
